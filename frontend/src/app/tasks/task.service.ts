2e8df87a45618111714633b99f4f2c310d0eece6
kathelan <mateuszdeptulski@gmail.com>
2020-12-01 22:57:43 +0100

    angular startup project, making get list method

---
+++ frontend/src/app/tasks/task.service.ts
@@ -0,0 +1,13 @@
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+
+@Injectable()
+export class TaskService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    getTasks() {
+        return this.http.get('/api/tasks');
+    }
+}
---
+++ frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {Task} from "../task.model";
+import {TaskService} from "../task.service";
+
+@Component({
+  selector: 'app-tasks-list',
+  templateUrl: './tasks-list.component.html',
+  styleUrls: ['./tasks-list.component.css']
+})
+export class TasksListComponent implements OnInit {
+
+    tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit()  {
+      return this.taskService.getTasks().subscribe(
+          (tasks: any[]) => {
+              this.tasks = tasks
+          },
+          (error) => console.log(error)
+      )
+  }
+
+  getDueDateLabel(task:Task) {
+      return task.completed ? 'badge-success' : 'badge-primary';
+  }
+
+  onTaskChange(event, task) {
+      // this.taskService.saveTask(task, event.target.checked).subscribe();
+      console.log('Task has changed');
+  }
+
+}
